2e4c3c158438c4a22d032dd0ed8b58c13c1975b4
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-04-27 13:39:04 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 // components
 import Pocetna from './pages/Pocetna.jsx'
-import Usluge from './pages/Usluge.jsx'
 import Kontakt from './pages/Kontakt.jsx'
+import Usluge from './pages/Usluge.jsx'
 // router
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+	RouterProvider,
+	createBrowserRouter,
+} from 'react-router-dom';
 
-const router = createBrowserRouter([{
-  path: '/',
-  element: <App />,
-  elementError: <div>error</div>,
-  children: [
-    {
-      path: '/',
-      element: <Pocetna />,
-    },
-    {
-      path: '/usluge',
-      element: <Usluge />,
-    },
-    {
-      path: '/kontakt',
-      element: <Kontakt />,
-    }
-  ],
-},
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		elementError: <div>error</div>,
+		children: [
+			{
+				path: '/',
+				element: <Pocetna />,
+			},
+			{
+				path: '/usluge',
+				element: <Usluge />,
+			},
+			{
+				path: '/kontakt',
+				element: <Kontakt />,
+			},
+		],
+	},
 ]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
