96984df4dd26aad7703ab971099c5473731cb226
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-04-27 13:54:50 +0200

    new router

--- src/main.jsx
+++ src/main.jsx
@@ -11,26 +11,21 @@ import Usluge from './pages/Usluge.jsx'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		elementError: <div>error</div>,
-		children: [
-			{
-				path: '/',
-				element: <Pocetna />,
-			},
-			{
-				path: '/usluge',
-				element: <Usluge />,
-			},
-			{
-				path: '/kontakt',
-				element: <Kontakt />,
-			},
-		],
-	},
-]);
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Pocetna />
+      },
+      {
+        path: '/test',
+        element: <div>RADI TEST</div>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
