5e2ff604f8743eb07d7715437f698b68aa74269d
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-04-27 14:11:58 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-      <h1>navbar</h1>
+      <Navbar />
       <Outlet />
     </>
   )
